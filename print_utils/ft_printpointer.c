79d2ee9e77848fff124cea8eb8b1a87c2682c484
Olivier Cartier <me@cestoliv.com>
2021-12-03 13:26:01 +0100

    pointer woking with every bonuses

--- print_utils/ft_printpointer.c
+++ print_utils/ft_printpointer.c
@@ -6,7 +6,7 @@
 /*   By: ocartier <ocartier@student.42lyon.f>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 16:44:10 by ocartier          #+#    #+#             */
-/*   Updated: 2021/12/03 11:16:54 by ocartier         ###   ########lyon.fr   */
+/*   Updated: 2021/12/03 13:22:41 by ocartier         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,19 +34,19 @@ static int	print_long_as_hex(long unsigned addr)
 	return (total);
 }
 
-static int  get_hex_size(long unsigned addr)
+static int	get_hex_size(long unsigned addr)
 {
-    int     total;
+	int	total;
 
-    total = 0;
-    if (addr >= 16)
-    {
-        total += get_hex_size(addr / 16);
-        total += get_hex_size(addr % 16);
-    }
-    else
-        total++;
-    return (total);
+	total = 0;
+	if (addr >= 16)
+	{
+		total += get_hex_size(addr / 16);
+		total += get_hex_size(addr % 16);
+	}
+	else
+		total++;
+	return (total);
 }
 
 int	ft_printpointer(void *ptr, t_opt opt)
@@ -56,18 +56,13 @@ int	ft_printpointer(void *ptr, t_opt opt)
 	int		len;
 
 	addr = (long unsigned)ptr;
-	total = 2;
-	len = get_hex_size(addr);
-	/*
-	if (addr == 0)
-		return (print_str("(nil)"));
-	*/
-	if (opt.min_width > 0)
-	{
-		while (len + total < opt.min_width)
-			total += print_char(' ');
-	}
-	write(1, "0x", 2);
+	total = 0;
+	len = get_hex_size(addr) + 2;
+	while (len + total < opt.min_width)
+		total += print_char(' ');
+	total += print_str("0x");
 	total += print_long_as_hex(addr);
+	while (total < opt.offset)
+		total += print_char(' ');
 	return (total);
 }

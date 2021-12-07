379e027d57786808390ee4fd7564928da55674f9
Olivier Cartier <me@cestoliv.com>
2021-12-07 11:55:56 +0100

    working on d and i

--- print_utils/ft_printnum.c
+++ print_utils/ft_printnum.c
@@ -6,7 +6,7 @@
 /*   By: ocartier <ocartier@student.42lyon.f>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 14:10:53 by ocartier          #+#    #+#             */
-/*   Updated: 2021/12/03 12:21:30 by ocartier         ###   ########lyon.fr   */
+/*   Updated: 2021/12/07 11:55:04 by ocartier         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,19 +51,88 @@ static int	number_of_digit(long num)
 	return (cur);
 }
 
+static int	get_values(char	*prefix, int *len_prec, t_opt *opt, long nl)
+{
+	int		len;
+
+	len = number_of_digit(nl);
+	*len_prec = len;
+	if (opt->precision > len)
+		*len_prec = opt->precision;
+	*prefix = '0';
+	if (opt->zero && opt->dot && opt->zero_offset > opt->precision)
+		*prefix = ' ';
+	if (!opt->zero)
+		*prefix = ' ';
+	if (opt->zero)
+		opt->min_width = opt->zero_offset;
+	if (opt->precision > opt->min_width)
+		opt->min_width = opt->precision;
+	return (len);
+}
+
+static int	print_di(int len, long nl, t_opt opt)
+{
+	int	total;
+
+	total = 0;
+	if (nl == 0 && opt.min_width && opt.min_width < len)
+		total += print_char(' ');
+	else if (nl == 0 && opt.dot && !opt.precision && opt.min_width >= len)
+		total += print_char(' ');
+	else if (!(nl == 0 && opt.dot && !opt.precision))
+		total += print_number(nl);
+	return (total);
+}
+
 int	ft_printnum(long nl, t_opt opt)
 {
 	int		total;
 	int		len;
 	char 	prefix;
+	int		len_prec;
 
 	total = 0;
-	len = number_of_digit(nl);
-	prefix = ' ';
-	if (opt.dot && opt.min_width == 0 && nl == 0)
-		return (0);
-	if (opt.dot)
-		prefix = '0';
+	len = get_values(&prefix, &len_prec, &opt, nl);
+
+	if (nl < 0 && prefix == '0')
+	{
+		total += print_char('-');
+		nl *= -1;
+		if (opt.dot)
+			len -= 2;
+		else
+			len -= 1;
+	}
+	/*
+	else if (nl < 0)
+	{
+		len++;
+		len_prec++;
+	}
+	*/
+
+	while (len_prec + total < opt.min_width)
+		total += print_char(prefix);
+	
+	if (nl < 0)
+	{
+		total += print_char('-');
+		nl *= -1;
+		if (opt.dot)
+			len -= 2;
+		else
+			len -= 1;
+	}
+
+	while (len + total < opt.min_width)
+		total += print_char('0');
+
+	total += print_di(len, nl, opt);
+	
+	while (total < opt.offset)
+		total += print_char(' ');
+	/*
 	if (opt.min_width > 0 && (!opt.minus || opt.dot))
 	{
 		if (opt.plus && nl >= 0)
@@ -89,5 +158,6 @@ int	ft_printnum(long nl, t_opt opt)
 		while (total < opt.min_width)
 			total += print_char(' ');
 	}
+	*/
 	return (total);
 }

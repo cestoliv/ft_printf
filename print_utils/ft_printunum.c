fecc6505b3c1ed5d60169d823b1e7b78c3d0e49b
Olivier Cartier <me@cestoliv.com>
2021-12-08 15:52:56 +0100

    u working and NORME OK

--- print_utils/ft_printunum.c
+++ print_utils/ft_printunum.c
@@ -1,13 +1,13 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printunum.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ocartier <ocartier@student.42lyon.f>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/01 14:11:31 by ocartier          #+#    #+#             */
-/*   Updated: 2021/12/02 16:53:52 by ocartier         ###   ########.fr       */
-/*                                                                            */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   ft_printunum.c									 :+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: ocartier <ocartier@student.42lyon.f>	   +#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2021/12/01 14:11:31 by ocartier		  #+#	#+#			 */
+/*   Updated: 2021/12/02 16:53:52 by ocartier		 ###   ########.fr	   */
+/*																			*/
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
@@ -44,18 +44,55 @@ static int	number_of_digit(unsigned long num)
 	return (cur);
 }
 
-int	ft_printunum(unsigned long nl, t_opt opt)
+static int	get_values(char	*prefix, int *lp, t_opt *opt, unsigned long num)
 {
-	int	total;
 	int	len;
 
+	len = number_of_digit(num);
+	*lp = len;
+	if (opt->precision > len)
+		*lp = opt->precision;
+	*prefix = '0';
+	if (opt->zero && opt->dot && opt->zero_offset > opt->precision)
+		*prefix = ' ';
+	if (!opt->zero)
+		*prefix = ' ';
+	if (opt->zero)
+		opt->min_width = opt->zero_offset;
+	if (opt->precision > opt->min_width)
+		opt->min_width = opt->precision;
+	return (len);
+}
+
+static int	print_u(int len, unsigned long num, t_opt opt)
+{
+	int	total;
+
 	total = 0;
-	if (opt.min_width > 0)
-	{
-		len = number_of_digit(nl);
-		while (len + total < opt.min_width)
-			total += print_char(' ');
-	}
-	total += print_unumber(nl);
+	if (num == 0 && opt.min_width && opt.min_width < len)
+		total += print_char(' ');
+	else if (num == 0 && opt.dot && !opt.precision && opt.min_width >= len)
+		total += print_char(' ');
+	else if (!(num == 0 && opt.dot && !opt.precision))
+		total += print_unumber(num);
+	return (total);
+}
+
+int	ft_printunum(unsigned long num, t_opt opt)
+{
+	int		total;
+	int		len;
+	int		len_prec;
+	char	prefix;
+
+	total = 0;
+	len = get_values(&prefix, &len_prec, &opt, num);
+	while (len_prec + total < opt.min_width)
+		total += print_char(prefix);
+	while (len + total < opt.min_width)
+		total += print_char('0');
+	total += print_u(len, num, opt);
+	while (total < opt.offset)
+		total += print_char(' ');
 	return (total);
 }
